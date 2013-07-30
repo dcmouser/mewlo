8d3249c91a281df116eab2faabbecaa3789d8c06
mouser <mouser@donationcoder.com>
2013-07-30 15:12:39 -0500

    minor changes to docs

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -25,7 +25,14 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+
+
+# ATTN: 7/30/13 jr - addign rst2pdf extension for pdf output add ", 'rst2pdf.pdfbuilder'"
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.ifconfig']
+
+
+
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
