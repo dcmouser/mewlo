be3ab7f905500cc3510fcf67c46cf11871b862e3
mouser <mouser@donationcoder.com>
2013-09-01 16:17:50 -0500

    added helper functions for creating setuptools entrypoint based egg plugins

---
+++ mewlo/externaltools/setuptoolhelpers.py
@@ -0,0 +1,53 @@
+"""
+setuptoolshelpers.py
+
+This file defines helper functions for the mewlo setuptools based test plugin, which is used to test one way of autodiscovering plugins for mewlo.
+
+"""
+
+
+
+
+def shelp_readfile_asjson(filepath):
+    """
+    Read a file and return json dictionary
+    :return: EFailure on error
+    """
+
+    # python libraries
+    import json
+
+
+    # open the file and load into a string
+    try:
+        # open file for reading, and read it into string
+        file = open(filepath, 'r')
+    except Exception as exp:
+        raise
+
+    # read the file
+    try:
+        jsonstr = file.read()
+    except Exception as exp:
+        raise
+    finally:
+        file.close()
+
+    # parse string as json
+    try:
+        jsondict = json.loads(jsonstr)
+    except Exception as exp:
+        raise
+
+    # success
+    return jsondict, None
+
+
+
+
+def dvalordef(adict, akey, adefault=None):
+    if (akey in adict):
+        return adict[akey]
+    return adefault
+
+
