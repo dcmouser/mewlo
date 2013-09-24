7108204142c3154366c0cfc3f42b4716f8a2ef8c
mouser <mouser@donationcoder.com>
2013-09-23 22:39:07 -0500

    cleaning up and renaming test directory

--- tests/demosites/test1/mpackages/readme.txt
+++
@@ -1,2 +0,0 @@
-Optional user-installed component extensions go here.
-These component extensions are isolated to ONLY this site.
--- tests/mewlosetuptoolsplugintest/__init__.py
+++
@@ -1,5 +0,0 @@
-# package
-"""
-This is the root directory of the mewlo egg.
-It contains a "mewlo" subdirectory where the actual code lives, and other top-level directories for documentation, etc.
-"""
